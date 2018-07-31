fac059c2e4b982719c10c24742d3722d10b958b0
Ashari Juang <juang@qiscus.co>
2018-07-31 15:38:40 +0700

    send message

--- QiscusCore/QiscusComment.swift
+++ QiscusCore/QiscusComment.swift
@@ -11,4 +11,16 @@ import Foundation
 // MARK: Comment Management
 extension QiscusCore {
     
+    public func sendMessage(roomID id: String, comment: QComment, completion: @escaping (QComment?, QError?) -> Void) {
+        QiscusCore.network.postComment(roomId: id, type: CommentType(rawValue: comment.type)!, comment: comment.message, payload: "", extras: "", uniqueTempId: comment.uniqueTempId) { (result, error) in
+            completion(result,nil)
+        }
+    }
+    
+    public func loadComments(roomID id: String, completion: @escaping ([QComment]?, QError?) -> Void) {
+        QiscusCore.network.loadComments(roomId: id) { (comments, error) in
+            completion(comments,nil)
+        }
+    }
+    
 }
