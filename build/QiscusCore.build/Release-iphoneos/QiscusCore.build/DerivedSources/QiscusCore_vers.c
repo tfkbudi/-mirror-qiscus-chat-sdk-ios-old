3bec8987fe1759678ee49f6dd3facfee82ece44c
Ashari Juang <asharijuang@me.com>
2018-08-17 12:58:49 +0700

    remove build

--- build/QiscusCore.build/Release-iphoneos/QiscusCore.build/DerivedSources/QiscusCore_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char QiscusCoreVersionString[];
- extern const double QiscusCoreVersionNumber;
-
- const unsigned char QiscusCoreVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:QiscusCore  PROJECT:QiscusCore-1" "\n";
- const double QiscusCoreVersionNumber __attribute__ ((used)) = (double)1.;
