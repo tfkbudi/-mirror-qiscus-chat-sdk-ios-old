9df8c8ba2c4dbacde1b2fccfdefea45c9101d4a6
Ashari Juang <asharijuang@me.com>
2018-09-07 17:54:24 +0700

    create build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-FRAMEWORK=ObjectiveRocks
+# Note: this file is originally based upon
+# https://gkbrown.org/2017/10/11/creating-a-universal-framework-in-xcode-9/
+# Since it includes simulator slices as well, these will need to be
+# stripped out prior to the apps being submitted to the app store
+
+FRAMEWORK=QiscusCore
 BUILD=build
 FRAMEWORK_NAME_WITH_EXT=$FRAMEWORK.framework
 DSYM_NAME_WITH_EXT=$FRAMEWORK_NAME_WITH_EXT.dSYM
@@ -8,7 +13,6 @@ IOS_ARCHIVE_FRAMEWORK_PATH=$BUILD/$IOS_ARCHIVE_DIR/Products/Library/Frameworks/$
 IOS_ARCHIVE_DSYM_PATH=$BUILD/$IOS_ARCHIVE_DIR/dSYMs
 IOS_SIM_DIR=Release-iphonesimulator
 IOS_UNIVERSAL_DIR=Release-universal-iOS
-MACOS_ARCHIVE_DIR=Release-macos-archive
 
 echo "### Cleaning up after old builds"
 rm -Rf $BUILD
@@ -20,11 +24,12 @@ if ! [ -x "$(command -v xcpretty)" ]; then
 fi
 
 # iOS
+echo "### Bismillahirrahmanirrahim."
 echo "### BUILDING FOR iOS"
 echo "### Building for device (Archive)"
-xcodebuild archive -workspace ObjectiveRocks.xcworkspace -scheme ObjectiveRocks-iOS -sdk iphoneos -archivePath $BUILD/Release-iphoneos.xcarchive OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode | xcpretty
+xcodebuild archive -workspace QiscusCore.xcworkspace -scheme QiscusCore -sdk iphoneos -archivePath $BUILD/Release-iphoneos.xcarchive OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode | xcpretty
 echo "### Building for simulator (Release)"
-xcodebuild build -workspace ObjectiveRocks.xcworkspace -scheme ObjectiveRocks-iOS -configuration Release -sdk iphonesimulator SYMROOT=$BUILD OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode | xcpretty
+xcodebuild build -workspace QiscusCore.xcworkspace -scheme QiscusCore -configuration Release -sdk iphonesimulator SYMROOT=$BUILD OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode | xcpretty
 echo "### Copying framework files"
 mv $BUILD/Release-iphoneos.xcarchive $BUILD/$IOS_ARCHIVE_DIR
 mkdir -p $BUILD/$IOS_UNIVERSAL_DIR
@@ -37,23 +42,15 @@ echo "### lipo'ing the iOS dSYMs together into a universal dSYM"
 DSYM_PATH=$DSYM_NAME_WITH_EXT/Contents/Resources/DWARF/$FRAMEWORK
 lipo -create $IOS_ARCHIVE_DSYM_PATH/$DSYM_PATH $BUILD/$IOS_SIM_DIR/$DSYM_PATH  -output $BUILD/$IOS_UNIVERSAL_DIR/$DSYM_PATH
 
-# macOS
-echo "### BUILDING FOR macOS"
-echo "### Building for device (Archive)"
-xcodebuild archive -workspace ObjectiveRocks.xcworkspace -scheme ObjectiveRocks -archivePath $BUILD/Release-macos.xcarchive OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode | xcpretty
-mv $BUILD/Release-macos.xcarchive $BUILD/$MACOS_ARCHIVE_DIR
 
 # Rename and zip
-echo "### Copying iOS and macOS files into zip directory"
+echo "### Copying iOS files into zip directory"
 ZIP_DIR=$BUILD/zip
 mkdir $ZIP_DIR
 cp -RL LICENSE $ZIP_DIR
 mkdir $ZIP_DIR/iOS
-mkdir $ZIP_DIR/macOS
 cp -RL $BUILD/$IOS_UNIVERSAL_DIR/$FRAMEWORK_NAME_WITH_EXT $ZIP_DIR/iOS/$FRAMEWORK_NAME_WITH_EXT
 cp -RL $BUILD/$IOS_UNIVERSAL_DIR/$DSYM_NAME_WITH_EXT $ZIP_DIR/iOS/$DSYM_NAME_WITH_EXT
-cp -RL $BUILD/$MACOS_ARCHIVE_DIR/Products/Library/Frameworks/$FRAMEWORK_NAME_WITH_EXT $ZIP_DIR/macOS/$FRAMEWORK_NAME_WITH_EXT
-cp -RL $BUILD/$MACOS_ARCHIVE_DIR/dSYMs/$DSYM_NAME_WITH_EXT $ZIP_DIR/macOS/$DSYM_NAME_WITH_EXT
 cd $ZIP_DIR
-zip -r ObjectiveRocks.zip LICENSE iOS/$FRAMEWORK_NAME_WITH_EXT iOS/$DSYM_NAME_WITH_EXT macOS/$FRAMEWORK_NAME_WITH_EXT macOS/$DSYM_NAME_WITH_EXT
-echo "### Zipped resulting frameworks and dSYMs to $ZIP_DIR/ObjectiveRocks.zip"
+zip -r QiscusCore.zip LICENSE iOS/$FRAMEWORK_NAME_WITH_EXT iOS/$DSYM_NAME_WITH_EXT
+echo "### Zipped resulting frameworks and dSYMs to $ZIP_DIR/QiscusCore.zip"
