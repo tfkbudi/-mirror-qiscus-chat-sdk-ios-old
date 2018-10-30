3d86e1946347348695700e4b7d201602da3467cf
asharijuang <asharijuang@me.com>
2018-10-30 10:43:45 +0700

    update build

--- build.sh
+++ build.sh
@@ -102,6 +102,7 @@ cp -RL LICENSE $PUBLISH
 cp -RL README.md $PUBLISH
 git add .
 git commit -m "finish build for cocoapod"
+rm -rf $BUILD
 
 say -v veena Do you want to publish to github?
 echo -n "\033[31m Mau sekalian di publish ke github (y/n)? \033[0m\n"
@@ -125,4 +126,3 @@ git commit -m "update new build"
 git push origin master
 echo "\033[35m Finish update cocoapod repo \n Alhamdulillah 🎉 🎉 🎉 \033[0m\n"
 say -v veena Horree 🎉, - see you next build - Ashari
-rm -rf $BUILD
