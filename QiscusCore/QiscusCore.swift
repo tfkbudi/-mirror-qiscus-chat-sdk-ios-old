3633bfcc506b550bc1569b2d3ba9cf15b4b17b45
ariefnurputranto <arief@qiscus.co>
2019-04-02 14:48:10 +0700

    update version 0.2.14

--- QiscusCore/QiscusCore.swift
+++ QiscusCore/QiscusCore.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public class QiscusCore: NSObject {
-    public static let qiscusCoreVersionNumber:String = "0.2.13"
+    public static let qiscusCoreVersionNumber:String = "0.2.14"
     class var bundle:Bundle{
         get{
             let podBundle = Bundle(for: QiscusCore.self)
